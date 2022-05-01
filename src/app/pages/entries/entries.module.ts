e56176de68bff0d62b811f226011c7dd4f04ed05
thiagocessin <thiagoacessin@gmail.com>
2022-05-01 12:17:51 -0300

    Criando campos form de lancamentos

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -6,12 +6,17 @@ import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { CalendarModule } from "primeng/calendar";
+import { IMaskModule } from "angular-imask";
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
     CommonModule,
     EntriesRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CalendarModule,
+    IMaskModule
+
   ]
 })
 export class EntriesModule { }
