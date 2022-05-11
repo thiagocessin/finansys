f8f70614432bbe6ecf108f6f3ec91bbc62c66578
thiagocessin <thiagoacessin@gmail.com>
2022-05-11 14:30:00 -0300

    Criando baseResourceModel

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,6 +1,7 @@
+import { BaseResourceModel } from "src/app/shared/models/base-resource";
 import { Category } from "../../categories/shared/category.model";
 
-export class Entry{
+export class Entry extends BaseResourceModel{
 
   constructor(public id?:number,
               public name?:string,
@@ -12,6 +13,7 @@ export class Entry{
               public categoryId?:number,
               public category?:Category){
 
+                super();
   }
 
   static types ={
